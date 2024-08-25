8969eeda1938e70076af9ebbf3c08cd3fd9ea9c6
kasididd <kasidid.wan@gmail.com>
2024-08-25 19:50:08 +0700

    update

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -8,7 +8,7 @@ export class BaseController<TService extends BaseControllerServiceModel<TEntity>
     this.service = service;
   }
 
-  @Get('/all')
+  @Get()
   async findAll() {
     return await this.service.findAll();
   }
