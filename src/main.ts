ca69695b6921b8ee76e927bfec17fd89bcc89678
kasididd <kasidid.wan@gmail.com>
2024-09-08 10:31:06 +0700

    add cors 3001

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // เปิดใช้งาน CORS
+  app.use(cors({
+    origin: 'http://localhost:3001',  // อนุญาตเฉพาะจาก localhost:3001
+  }));
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
